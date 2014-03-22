09ddbc83b71db5230284a820ad520097c26a802e
TunedMystic <jadoonanansandeep@gmail.com>
2014-03-22 06:56:54 -0400

    Some issues with word animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// 63650395
+// Generates a random number
 function ran(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -13,7 +13,8 @@ function toDegrees(radians) {
   return radians * 180 / Math.PI;
 };
 
-/// Takes an element and applies a css transition to it
+/// Takes an element and applies a css transition to it.
+/// VAR: "thing" -> css selector
 function randomShoot(thing) {
   var angle = toRadians( ran(0, 360) );
   var distance = ran(140, 160);
@@ -37,30 +38,34 @@ function randomShoot(thing) {
     );
   return "(" + theX + "px, " + theY + "px)";*/
   var transformValue = "(" + theX + "px, " + theY + "px)";
+  console.log(transformValue);
   $(thing).css("transform", "translate" + transformValue);
 }
 
+/// VAR: "thing" -> css selector
 function explode(thing) {
+   console.log("Explode start");
+
+   if( $("#renderedText").children().length == 0 ) {
+      console.log("NO CHILDREN");
+   }
+   else {
+      console.log("Children YES!!");
+   }
+
    $(thing + " span").each(function(i, el){
      /// Apply a random translation to each span
      //console.log( randomShoot(el) );
      randomShoot(el);
    });
    ///$("#word").trigger("webkitAnimationEnd");
+   console.log("Explode done");
 }
 
-var c;
-function explodeForever() {
-  c = setInterval(function(){
-    explode("#word");
-  }, 1500);
-}
-function stopExplode() {
-  clearInterval(c);
-}
 
 /// Takes an element and replaces every
 /// whitespace with an ' &nbsp; ' element.
+/// VAR: "thing" -> css selector
 function fillWhitespace(thing) {
    var txt = $(thing).text();
    var txt2 = "";
@@ -75,7 +80,107 @@ function fillWhitespace(thing) {
    return txt2;
 }
 
-var p;
+/// VAR: "thing" -> css selector
+function letter(thing) {
+   console.log("Lettering start");
+   $(thing).html( fillWhitespace($(thing)) );
+   $(thing).lettering().children("span").addClass("changableText");
+   console.log("Lettering done");
+   return;
+}
+
+/// Takes the inputbox text and puts in the 'big-font' section
+function updateInput() {
+   if( $("#explodeInput").val() == "" ) {
+      $("#renderedText").text("...This will explode!!");
+   }
+   else {
+      $("#renderedText").text( $("#explodeInput").val() );
+   }
+}
+
+var tag = "#renderedText";
+
+/// Explodes the 'big-font' text
+function explodeText() {
+   /// If the input box actually has something...
+   if( $("#explodeInput").val() == "" ) {
+      console.log("an input must be entered...");
+   }
+   else {
+      /*letter("#renderedText");
+      setTimeout(function() {
+         explode("#renderedText")
+      }, 100);*/
+      ///$.when( letter("#renderedText") ).then( explode("#renderedText") );
+      /*$( letter("#renderedText") ).promise().done(function() {
+         explode("#renderedText");
+      });
+      $.when( letter("#renderedText") ).done(function() {
+         explode("#renderedText")
+      });*/
+
+      
+
+
+      /*letter("#renderedText");
+
+       /// Start rumbling
+      $("#renderedTextP").trigger("startRumble");
+
+      /// Wait half a second, then stop it
+      setTimeout(function() {
+         $("#renderedTextP").trigger("stopRumble")
+      }, 500);
+
+      explode("#renderedText");*/
+
+      setTimeout(function() {
+         letter("#renderedText");
+      }, 40);
+
+      setTimeout(function() {
+         console.log("start start");
+         $("#renderedTextP").trigger("startRumble");
+         console.log("start stop");
+      }, 50);
+
+      setTimeout(function() {
+         console.log("stop start");
+         $("#renderedTextP").trigger("stopRumble");
+         console.log("stop stop");
+      }, 550);
+
+      setTimeout(function() {
+         explode("#renderedText");
+      }, 560);
+
+   }
+}
+
+var exp = function() {
+   explodeText();
+}
+
+var xp = function() {
+   explode("#renderedText");
+}
+
+var lt = function() {
+   letter("#renderedText");
+}
+
+$("#explodeInput").bind("input", updateInput);
+
+$("#renderedTextP").jrumble({
+   x: 2,
+   y: 4,
+   rotation: 5
+});
+
+/// $("#explodeInput").bind("input", updateInput);
+
+/*var p;
 $(document).ready(function() {
   $("#renderedText").lettering().children("span").addClass("changableText");
   p = $("#renderedText span")[0];
@@ -83,4 +188,4 @@ $(document).ready(function() {
   $("#renderedText span:eq(0)").on("webkitTransitionEnd", function() {
     console.log("...The animation has ended");
   });
-});
+});*/
